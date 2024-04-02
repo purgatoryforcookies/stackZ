d7bfde843991d9eb3cd58d7b0229ddfe02c5df57
Max Caroe <max.caroe@gmail.com>
2024-04-02 23:11:13 +0300

    better, less intrusive search box

--- src/renderer/src/components/TerminalUI/TerminalUI.tsx
+++ src/renderer/src/components/TerminalUI/TerminalUI.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react'
 import { TerminalUIEngine } from '@renderer/service/TerminalUIEngine'
 import { Input } from '@renderer/@/ui/input'
 import { useTaalasmaa } from '@renderer/hooks/useTaalasmaa'
+import { CaretLeftIcon } from '@radix-ui/react-icons'
 
 type TerminalUIProps = {
     engine: TerminalUIEngine | undefined
@@ -10,6 +11,7 @@ type TerminalUIProps = {
 function TerminalUI({ engine }: TerminalUIProps) {
     const terminalRef = useRef<HTMLDivElement>(null)
     const [search, setSearch] = useState<string>('')
+    const [searchOpen, setSearchOpen] = useState<boolean>(false)
 
     const { w, h } = useTaalasmaa(terminalRef)
 
@@ -29,8 +31,6 @@ function TerminalUI({ engine }: TerminalUIProps) {
         if (engine) engine.search(search)
     }
 
-    const isCompact = w && w < 500
-
     return (
         <>
             <form
@@ -38,10 +38,13 @@ function TerminalUI({ engine }: TerminalUIProps) {
                 onSubmit={(e) => {
                     e.preventDefault(), findNext()
                 }}
+                className='absolute top-1 right-1 z-10'
             >
+                <CaretLeftIcon className="absolute top-2 right-1 size-5 z-10 text-white hover:cursor-pointer"
+                    onClick={() => setSearchOpen(!searchOpen)} />
                 <Input
                     type="text"
-                    className={`text-white absolute top-1 right-1 z-10 truncate transition-width duration-500 ease-in-out backdrop-blur-lg ${isCompact ? 'w-[5rem]' : 'w-[30rem]'}`}
+                    className={`text-white truncate transition-width duration-500 ease-in-out backdrop-blur-lg ${searchOpen ? 'w-[1rem]' : 'w-[20rem]'}`}
                     placeholder="Highlight..."
                     value={search}
                     onChange={(e) => setSearch(e.target.value)}
